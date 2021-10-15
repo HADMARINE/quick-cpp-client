b8ae3a8ab58b5dd759bdf46a99baa88edf9fcc29
HADMARINE <contact@hadmarine.com>
2021-10-16 00:09:07 +0900

    Update client code

---
+++ quick-cpp-client/include/http_client.hh
@@ -0,0 +1,25 @@
+#include <nlohmann/json.hpp>
+#include <string>
+#include <optional>
+#include <cpprest/http_client.h>
+
+
+namespace quick_client {
+    namespace http {
+        using json = nlohmann::json;
+        using namespace std;
+        json http_get(string url, unordered_map<string, string>);
+        json http_post();
+        json http_patch();
+        json http_put();
+        json http_delete();
+        json primitive_requester();
+
+        struct request_options {
+            optional<web::http::methods> method,
+            optional<unordered_map<string,string>> headers,
+            optional<string> url,
+        };
+
+    };
+};
